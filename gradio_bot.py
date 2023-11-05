3477c2a6a88f2b42e8b18443a20b7e4f31a1c90d
Nikita Trukhanov <darth.lynxxx@gmail.com>
2023-11-05 00:38:21 +0000

    Make gradio to work with the backend

--- gradio_bot.py
+++ gradio_bot.py
@@ -1,27 +1,34 @@
+from anthropic import Anthropic
 import gradio as gr
 import time
 import random
-problem_bank = {'Coin toss':   {'problem': "toss a coin to your witcher\noh valley of plenty"},
-               'LeetCode': {'problem': "write a python code for some problem"}}
+#problem_bank = {'Coin toss':   {'problem': "toss a coin to your witcher\noh valley of plenty"},
+#               'LeetCode': {'problem': "write a python code for some problem"}}
+
+from dotenv import load_dotenv
+from prompts import *
+from tutor import Tutor
+
+load_dotenv()
+anthropic = Anthropic()
 
 class Responser:
     def __init__(self):
         super().__init__()
-        self.state = []
-    def reset(self):
-        self.state = []
+        self.tutor = None
+    def reset(self, problem_name):
+        self.tutor = Tutor(anthropic, PROBLEMS[problem_name], MODELS[problem_name])
     def respond(self, message):
-        self.state.append(message)
-        resp = str(random.random())
-        self.state.append(resp)
+        resp = self.tutor.run(message)
         return resp
 
 def run_demo(responser):
     def new_statement(dropdown_value):
-        return gr.Textbox(problem_bank[dropdown_value]['problem'])
+        #return gr.Textbox(problem_bank[dropdown_value]['problem'])
+        return gr.Textbox(PROBLEMS[dropdown_value]), gr.Textbox(value="", interactive=True), gr.Button(interactive=True)
 
     def generate_output(*args):
-        output_text = "Hello, dumbass!"
+        output_text = "Hello!"
         return output_text
 
     def add_text(history, text):
@@ -40,17 +47,17 @@ def run_demo(responser):
 
             # column for inputs
             with gr.Column():
-                drop = gr.Dropdown(choices=list(problem_bank.keys()), label = "Choose a problem")
+                drop = gr.Dropdown(choices=list(PROBLEMS.keys()), label = "Choose a problem")
                 statement = gr.Textbox("",interactive=False, show_label=False)
 
             # column for outputs
             with gr.Column():
                 chat = gr.Chatbot(None, label="Chat")
-                message = gr.Textbox("", interactive=True, label="Type your solution:")
-                send_button = gr.Button("Send", interactive=True)
+                message = gr.Textbox("", interactive=False, label="Type your solution:")
+                send_button = gr.Button("Send", interactive=False)
 
-        drp_chng = drop.change(new_statement, drop, statement)
-        drp_chng = drp_chng.then(responser.reset)
+        drp_chng = drop.change(new_statement, drop, [statement, message, send_button])
+        drp_chng = drp_chng.then(responser.reset, drop)
         drp_chng.then(clear_chat, outputs = [chat,message, send_button])
         btn_click = send_button.click(add_text, [chat, message], [chat,message, send_button])
 
